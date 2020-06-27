d8bb9c3e93e34acfbd5ce316834162f5d8f07b99
Craig McQueen <craig@mcqueen.id.au>
2020-06-27 22:57:34 +1000

    Add LFSR88 and LFSR113

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,21 @@ pub struct KISS {
     shr3: Shr3,
 }
 
+#[derive(Debug)]
+pub struct LFSR88 {
+    z1: u32,
+    z2: u32,
+    z3: u32,
+}
+
+#[derive(Debug)]
+pub struct LFSR113 {
+    z1: u32,
+    z2: u32,
+    z3: u32,
+    z4: u32,
+}
+
 
 impl Cong {
     pub fn new() -> Cong {
@@ -132,6 +147,83 @@ impl KISS {
         self.mwc2.next();
         self.cong.next();
         self.shr3.next();
+
+        self.current()
+    }
+}
+
+fn next_z(z: u32, a: u8, b: u8, c: u8, mask: u32) -> u32 {
+    let b = ((z << a) ^ z) >> b;
+    ((z & mask) << c) ^ b
+}
+
+impl LFSR88 {
+    const Z1_MIN: u32 = 2;
+    const Z2_MIN: u32 = 8;
+    const Z3_MIN: u32 = 16;
+
+    pub fn new() -> LFSR88 {
+        LFSR88 {
+            z1: 0xFFFFFFFF,
+            z2: 0xFFFFFFFF,
+            z3: 0xFFFFFFFF,
+        }
+    }
+    pub fn next_z1(&mut self) {
+        self.z1 = next_z(self.z1, 13, 19, 12, 0xFFFFFFFE);
+    }
+    pub fn next_z2(&mut self) {
+        self.z2 = next_z(self.z2, 2, 25, 4, 0xFFFFFFF8);
+    }
+    pub fn next_z3(&mut self) {
+        self.z3 = next_z(self.z3, 3, 11, 17, 0xFFFFFFF0);
+    }
+    pub fn current(&self) -> u32 {
+        self.z1 ^ self.z2 ^ self.z3
+    }
+    pub fn next(&mut self) -> u32 {
+        self.next_z1();
+        self.next_z2();
+        self.next_z3();
+
+        self.current()
+    }
+}
+
+impl LFSR113 {
+    const Z1_MIN: u32 = 2;
+    const Z2_MIN: u32 = 8;
+    const Z3_MIN: u32 = 16;
+    const Z4_MIN: u32 = 128;
+
+    pub fn new() -> LFSR113 {
+        LFSR113 {
+            z1: 0xFFFFFFFF,
+            z2: 0xFFFFFFFF,
+            z3: 0xFFFFFFFF,
+            z4: 0xFFFFFFFF,
+        }
+    }
+    pub fn next_z1(&mut self) {
+        self.z1 = next_z(self.z1, 6, 13, 18, 0xFFFFFFFE);
+    }
+    pub fn next_z2(&mut self) {
+        self.z2 = next_z(self.z2, 2, 27, 2, 0xFFFFFFF8);
+    }
+    pub fn next_z3(&mut self) {
+        self.z3 = next_z(self.z3, 13, 21, 7, 0xFFFFFFF0);
+    }
+    pub fn next_z4(&mut self) {
+        self.z4 = next_z(self.z4, 3, 12, 13, 0xFFFFFF80);
+    }
+    pub fn current(&self) -> u32 {
+        self.z1 ^ self.z2 ^ self.z3 ^ self.z4
+    }
+    pub fn next(&mut self) -> u32 {
+        self.next_z1();
+        self.next_z2();
+        self.next_z3();
+        self.next_z4();
 
         self.current()
     }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ fn main() {
     //let mut s = simplerandom::Shr3::new();
     //let mut s = simplerandom::Cong::new();
     //let mut s = simplerandom::MWC2::new();
-    let mut s = simplerandom::KISS::new();
+    //let mut s = simplerandom::KISS::new();
+    let mut s = simplerandom::LFSR88::new();
+    //let mut s = simplerandom::LFSR113::new();
     s.next();
     let s_result = s.next();
     println!("simplerandom {}, {:?}", s_result, s);
