51b4b236e3d5e576173cc3d35f792b0d0a5d38d6
Craig McQueen <craig@mcqueen.id.au>
2020-07-12 13:31:03 +1000

    Test rand::Rng .gen() function (commented out)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use rand_core::RngCore;
+//use rand::Rng;
 
 fn main() {
     //let mut s = simplerandom::Cong::new(1);
     //let mut s = simplerandom::SHR3::new(1);
     //let mut s = simplerandom::MWC1::new(1, 2);
     //let mut s = simplerandom::MWC2::new(1, 2);
-    //let mut s = simplerandom::KISS::new(1, 2, 3, 4);
+    let mut s = simplerandom::KISS::new(1, 2, 3, 4);
     //let mut s = simplerandom::MWC64::new(1, 2);
-    let mut s = simplerandom::KISS2::new(1, 2, 3, 4);
+    //let mut s = simplerandom::KISS2::new(1, 2, 3, 4);
     //let mut s = simplerandom::LFSR88::new(1, 2, 3);
     //let mut s = simplerandom::LFSR113::new(1, 2, 3, 4);
     for _ in 0..4 {
-        let s_result = s.next_u32();
-        println!("{}, {:?}", s_result, s);
+        println!("{}, {:?}", s.next_u32(), s);
+        //println!("{}, {:?}", s.gen::<u32>(), s);
+        //println!("{:?}", s.gen::<(f64)>());
     }
 }
