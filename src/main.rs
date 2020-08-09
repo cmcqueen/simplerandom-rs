34d8daf4d364e2c13e3cd98a65a953bd41892846
Craig McQueen <craig@mcqueen.id.au>
2020-08-10 00:01:05 +1000

    JumpAhead for SHR3

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use simplerandom::RngJumpAhead;
 
 fn test_new_and_next_u32() {
     //let mut s = simplerandom::Cong::new(1);
-    //let mut s = simplerandom::SHR3::new(1);
+    let mut s = simplerandom::SHR3::new(1);
     //let mut s = simplerandom::MWC1::new(1, 2);
     //let mut s = simplerandom::MWC2::new(1, 2);
     //let mut s = simplerandom::KISS::new(1, 2, 3, 4);
-    let mut s = simplerandom::MWC64::new(1, 2);
+    //let mut s = simplerandom::MWC64::new(1, 2);
     //let mut s = simplerandom::KISS2::new(1, 2, 3, 4);
     //let mut s = simplerandom::LFSR88::new(1, 2, 3);
     //let mut s = simplerandom::LFSR113::new(1, 2, 3, 4);
